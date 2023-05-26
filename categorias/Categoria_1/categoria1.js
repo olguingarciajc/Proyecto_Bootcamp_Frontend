17b4046351a48b89e97a77873a7b9f9c6290b398
olguingarciajc <112455529+olguingarciajc@users.noreply.github.com>
2023-05-25 20:20:55 -0600

    -m
    se agregó modulo de formulario en la categoria 1

--- categorias/Categoria_1/categoria1.js
+++ categorias/Categoria_1/categoria1.js
@@ -13,78 +13,59 @@ cerrar.addEventListener("click", () => {
 
 let products = [
     {
-        id: 1,
-        title: "Curso de JS",
-        precio: 100,
-        descripcion: "Buen curso"
+    id: 1,
+    title: "Curso de JS",
+    precio: 100,
+    descripcion: "Buen curso"
     },
     {
-        id: 2,
-        title: "Curso de HTML",
-        precio: 100,
-        descripcion: "Super curso"
-    },
-    [
-    {
-        id: 3,
-        title: "Caja Grande",
-        precio: 100,
-        descripcion: "Buena Caja Grande"
-    },
-    {
-        id: 4,
-        title: "Caja Mediana",
-        precio: 100,
-        descripcion: "Buena Caja Mediana"
-    },
-    {
-        id: 5,
-        title: "Caja Chica",
-        precio: 100,
-        descripcion: "Buena Caja Chica"
+    id: 2,
+    title: "Curso de HTML",
+    precio: 100,
+    descripcion: "Super curso"
     }
     ]
-].flat(Infinity)
 
+    console.log(products);
 
+    let selectControl = document.querySelector("#products-select");
 
-let selectControl = document.querySelector("#products-select");
-
-products.forEach(
-    function (product) {
-        let option = document.createElement("option");
-        option.value = product.id;
-        option.innerHTML = product.title;
-        selectControl.appendChild(option);
-
+    products.forEach(
+    function(product){
+    let option = document.createElement("option");
+    option.value = product.id;
+    option.innerHTML = product.title;
+    selectControl.appendChild(option);
+    
     }
-);
+    );
 
-let form = document.querySelector("#products");
+    let form = document.querySelector("#products");
 
-form.addEventListener("submit", function (ev) {
+    form.addEventListener("submit",function(ev){
     ev.preventDefault();
     var data = new FormData(event.target);
-
+    
     let productId = selectControl.value;
-    let selectedProduct = products.find(function (product) {
-        return product.id == productId;
+    let selectedProduct=  products.find(function(product){
+    return product.id == productId;
+    });
+    
+    data.append("product-title", selectedProduct.title);
+    
+    fetch(form.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+    }).then(()=> alert("Correo enviado"));
+    
     });
 
-    data.append("product-title", selectedProduct.title);
+    let button = form.querySelector("button");
 
-    fetch(form.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(() => alert("Correo enviado"));
-
-});
-
-let button = form.querySelector("button");
-
-button.addEventListener("click", function (ev) {
+    button.addEventListener("click",function(ev){
     button.style.backgroundColor = "red";
-})
+    })
+
